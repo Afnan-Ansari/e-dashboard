e0de3e901478832c910ff528c4d6ef4ec984f0ad
Afnan-Ansari <afnanansari88896@gmail.com>
2022-09-23 16:31:05 +0530

    second commit

--- src/component/AddProduct.js
+++ src/component/AddProduct.js
@@ -15,7 +15,6 @@ const AddProduct = () => {
             return false;
         }
 
-
         console.warn(name, price, category, company);
         const userId = JSON.parse(localStorage.getItem('user'))._id;
         let result = await fetch('http://localhost:5000/add-product', {
