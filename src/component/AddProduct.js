dc60c7506625df1c1a708d4282b927c4cc1c473c
Afnan-Ansari <afnanansari88896@gmail.com>
2022-09-29 15:12:00 +0530

    second commit

--- src/component/AddProduct.js
+++ src/component/AddProduct.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import {useNavigate} from 'react-router-dom';
 
 const AddProduct = () => {
     const [name, setName] = useState('');
@@ -6,6 +7,7 @@ const AddProduct = () => {
     const [category, setCategory] = useState('');
     const [company, setCompany] = useState('');
     const [error, setError] = useState(false);
+    const navigate = useNavigate();
 
     const addProduct = async () => {
 
@@ -26,6 +28,7 @@ const AddProduct = () => {
             }
         });
         result = await result.json();
+        navigate('/');
         console.warn(result);
     }
     return (
